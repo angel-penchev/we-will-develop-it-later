eda905d5e7bbab0f8226a4e1723f600c4abd0fd3
Angel Penchev <angel.penchev.tsb@gmail.com>
2019-03-15 23:37:41 +0200

    Added the "ClassifierActivity" layout**
    **Important note: Instead of a "ImageView" element, the line below should contain a "com.camerakit.CameraKitView". However, it requires API 24 to be used which is currently unavailable to be used by my current device. You may check a fully working version of this over at the "android-test" branch. The issue will be resolved shortly! :D

--- app/src/main/java/com/wewilldevelopitlater/readability/ClassifierActivity.java
+++ app/src/main/java/com/wewilldevelopitlater/readability/ClassifierActivity.java
@@ -1,23 +1,16 @@
 package com.wewilldevelopitlater.readability;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class ClassifierActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_classifier);
 
         // Removes the Action Bar from the activity
         this.getSupportActionBar().hide();
-    }
-
-    public void nextScreen(View v) {
-        Intent intent = new Intent(MainActivity.this, ClassifierActivity.class);
-        startActivity(intent);
     }
 }

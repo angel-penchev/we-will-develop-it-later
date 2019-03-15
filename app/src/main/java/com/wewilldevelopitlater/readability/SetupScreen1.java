f4876e2ddb5662579ba1c336d2acd2c22d512793
Angel Penchev <angel.penchev.tsb@gmail.com>
2019-03-15 10:49:25 +0200

    Renamed MainActivity to SetupScreen and created the layout base.

---
+++ app/src/main/java/com/wewilldevelopitlater/readability/SetupScreen1.java
@@ -0,0 +1,38 @@
+package com.wewilldevelopitlater.readability;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.ViewFlipper;
+
+public class SetupScreen1 extends AppCompatActivity {
+    private ViewFlipper viewFlipper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setup_screen1);
+
+        // Removes the Action Bar from the activity
+        this.getSupportActionBar().hide();
+
+        // Creates the view flipper
+        viewFlipper = findViewById(R.id.view_flipper);
+    }
+
+    public void previousView(View v) {
+        viewFlipper.showPrevious();
+    }
+
+    public void nextView(View v) {
+        viewFlipper.showNext();
+    }
+
+    public void nextScreen(View v) {
+        Intent intent = new Intent(SetupScreen1.this, SetupScreen2.class);
+        startActivity(intent);
+    }
+}

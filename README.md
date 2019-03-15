2fe6998f53336b64505ab030d9b3ec858924ff37
Angel Penchev <angel.penchev.tsb@gmail.com>
2019-03-15 10:27:08 +0200

    Created "Setup Screen #1" activity

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # we-will-develop-it-later
-But maybe, just maybe, we will finish it one day :)
+Just a bit later...
--- app/src/main/java/com/wewilldevelopitlater/readability/MainActivity.java
+++ app/src/main/java/com/wewilldevelopitlater/readability/MainActivity.java
@@ -1,14 +1,32 @@
-ter
-        package com.wewilldevelopitlater.readability;
+package com.wewilldevelopitlater.readability;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
+    private ViewFlipper viewFlipper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Removes the Action Bar from the activity
+        this.getSupportActionBar().hide();
+
+        // Creates the view flipper
+        viewFlipper = findViewById(R.id.view_flipper);
+    }
+
+    public void previousView(View v) {
+        viewFlipper.showPrevious();
+    }
+
+    public void nextView(View v) {
+        viewFlipper.showNext();
     }
 }

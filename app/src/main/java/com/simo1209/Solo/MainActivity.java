a8563929e28f96c2dc06f27b7b346299e4eca235
simo1209 <simeongeorgiev09@gmail.com>
2019-03-17 00:45:34 +0200

    Recognition Attempt #3

---
+++ app/src/main/java/com/simo1209/Solo/MainActivity.java
@@ -0,0 +1,78 @@
+package com.simo1209.Solo;
+
+import android.graphics.BitmapFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.camerakit.CameraKitView;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
+
+public class MainActivity extends AppCompatActivity {
+
+    CameraKitView cameraKitView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cameraKitView = findViewById(R.id.camera);
+
+        Button capture = findViewById(R.id.capture);
+        TextView textView = findViewById(R.id.textView);
+
+        capture.setOnClickListener(v -> cameraKitView.captureImage((cameraKitView, bytes) -> {
+
+            FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(BitmapFactory.decodeByteArray(bytes, 0, bytes.length));
+
+            FirebaseVisionTextRecognizer detector = FirebaseVision.getInstance()
+                    .getOnDeviceTextRecognizer();
+
+            Task<FirebaseVisionText> result =
+                    detector.processImage(image)
+                            .addOnSuccessListener(firebaseVisionText -> {
+                                textView.setText(firebaseVisionText.getText());
+                                Log.d("Text",textView.getText().toString());
+                            }).addOnFailureListener(e -> Log.e("Text Recognition", "Failed"));
+
+        }));
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        cameraKitView.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        cameraKitView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        cameraKitView.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        cameraKitView.onStop();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        cameraKitView.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+}

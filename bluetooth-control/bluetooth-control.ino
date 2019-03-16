6607e44a000754293765d74cebaef8491c63ca9a
bobig6 <b.i.ivanov1@gmail.com>
2019-03-16 20:44:28 +0200

    Uploding the final arduino code

--- bluetooth-control/bluetooth-control.ino
+++ bluetooth-control/bluetooth-control.ino
@@ -1,5 +1,7 @@
 #include <SoftwareSerial.h>
 #include <Servo.h>
+#include<string.h>
+
 
 Servo ser1;
 Servo ser2;
@@ -9,26 +11,31 @@ void setup(){
   Serial.begin(38400);
   bt.begin(9600);
   ser1.attach(10);
-  ser1.write(0);
+  ser1.write(21.25);
   ser2.attach(9);
-  ser2.write(0);
+  ser2.write(170);
 }
 
 void loop(){
   
   if(bt.available()>0){
-    int val=bt.read() & ~(1<<7);
-    Serial.println(val,DEC);
-    Serial.println(val,BIN);
-    int pos1=val & 7;
-    int pos2=(val & 56)>>3;
-    Serial.print(pos1,BIN);
-    Serial.print(" ");
-    Serial.println(pos2,BIN);
-    Serial.print(pos1,DEC);
-    Serial.print(" ");
-    Serial.println(pos2,DEC);
-    ser1.write((pos1)*22.5);
-    ser2.write((pos2)*22.5);
+    String ms = bt.readString();
+
+    char c[4]; 
+    ms.toCharArray(c, 4);
+    Serial.println(ms);
+    int pos1 = c[0]-'0';
+    Serial.println(pos1);
+    ser1.write((pos1)*21.25);
+
+    
+    int pos2 = c[1]-'0';
+    Serial.println(c[1]);
+    Serial.println(pos2);
+    ser2.write((8-(pos2))*22.5);
   }
+    if(Serial.available()>0){
+    bt.println(Serial.read());
+  }
+  delay(20);
 }
